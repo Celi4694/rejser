dd74091de1bf848255881bdae79d170ba3e962df
Celine Asmussen <celi4694@stud.kea.dk>
2021-09-10 11:50:05 +0200

    Filtrering virker!!!!

--- destinationer.js
+++ destinationer.js
@@ -11,17 +11,14 @@ document.addEventListener("DOMContentLoaded", start);
 let byer;
 let filtrer = "alle";
 document.querySelector("#popup").style.display = "none";
-let kontinent = "alle";
-
-
-
+let kontinent;
 
 function start() {
-   // Oprettes en tom constant, fordi så har jeg en generel som gælder i alle functions nedenfor i scriptet //
-   const urlParams = new URLSearchParams(window.location.search);
-   kontinent = urlParams.get("kontinent");
-   let json;
-   console.log(kontinent);
+  // Oprettes en tom constant, fordi så har jeg en generel som gælder i alle functions nedenfor i scriptet //
+  const urlParams = new URLSearchParams(window.location.search);
+  kontinent = urlParams.get("kontinent");
+  let json;
+  console.log(kontinent);
 
   const filtrerKnapper = document.querySelectorAll("nav button");
   filtrerKnapper.forEach((knap) => knap.addEventListener("click", filtrerByer));
@@ -34,15 +31,14 @@ function start() {
 function filtrerNav() {
   const knapper = document.querySelectorAll("nav button");
   // console.log(knapper);
-  knapper.forEach(knap=>{
-    console.log(kontinent)
+  knapper.forEach((knap) => {
+    console.log(kontinent);
     if (knap.dataset.kontinent == kontinent) {
-      knap.style.display="inline";
+      knap.style.display = "inline";
     } else {
-      knap.style.display="none"
+      knap.style.display = "none";
     }
-  })
-  
+  });
 }
 
 function filtrerByer() {
@@ -50,16 +46,40 @@ function filtrerByer() {
   // console.log(filtrer);
   document.querySelector(".valgt").classList.remove("valgt");
   this.classList.add("valgt");
-  visByer();
   const header = document.querySelector("#alle_overskrift");
   header.textContent = this.textContent;
+  visByer();
 }
 
 async function hentdata() {
   const response = await fetch(url, options);
   byer = await response.json();
   // console.log(byer);
-  visByer();
+  visKontinenter();
+}
+
+function visKontinenter() {
+  const dest = document.querySelector("#liste");
+  const skabelon = document.querySelector("template").content;
+  dest.innerHTML = "";
+  //løb igennem array "byer"
+  // console.log(byer);
+
+  byer.forEach((enkleByer) => {
+    console.log(enkleByer.Kontinent, kontinent);
+    if (kontinent == enkleByer.Kontinent) {
+      const klon = temp.cloneNode(true).content;
+
+      klon.querySelector("img").src = "billeder/" + enkleByer.Billede + ".jpg";
+      klon.querySelector("h2").textContent = enkleByer.By;
+
+      klon
+        .querySelector("article")
+        .addEventListener("click", () => visPopUp(enkleByer));
+
+      dest.appendChild(klon);
+    }
+  });
 }
 
 function visByer() {
@@ -69,9 +89,9 @@ function visByer() {
   //løb igennem array "byer"
   // console.log(byer);
 
-  byer.forEach((enkleByer) => { 
+  byer.forEach((enkleByer) => {
     console.log(enkleByer.Kontinent, kontinent);
-    if (filtrer == enkleByer.Land || filtrer == "alle") {
+    if (filtrer == enkleByer.Land) {
       const klon = temp.cloneNode(true).content;
 
       klon.querySelector("img").src = "billeder/" + enkleByer.Billede + ".jpg";
@@ -109,37 +129,24 @@ function visPopUp(popUp) {
 
   // console.log(popUp);
 
-
-
-
-
-
-  
-
- 
-
   // Sitet synkroniserer med data fra json database
-  async function hentData() { 
-      const result = await fetch(url+kontinent, options);
-      land = await result.json();
-      // console.log(land);
-      visLand();
-      }
+  async function hentData() {
+    const result = await fetch(url + kontinent, options);
+    land = await result.json();
+    // console.log(land);
+    visLand();
+  }
 
   function visKontinent() {
-      document.querySelector(".navn").textContent = person.fornavn;
-      document.querySelector("img").src = "faces/" + person.billede;
-      document.querySelector(".email").textContent = person.email;
-      document.querySelector(".hobby").textContent ="Hobby: " + person.hobby;
-  }   
-  
-  document.querySelector("button").addEventListener("click",() => {window.history.back()});
+    document.querySelector(".navn").textContent = person.fornavn;
+    document.querySelector("img").src = "faces/" + person.billede;
+    document.querySelector(".email").textContent = person.email;
+    document.querySelector(".hobby").textContent = "Hobby: " + person.hobby;
+  }
 
-
-
-
-
-
+  document.querySelector("button").addEventListener("click", () => {
+    window.history.back();
+  });
 }
 
 hentdata();
